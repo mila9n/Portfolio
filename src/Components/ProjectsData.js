dfa4c7d788838b0653d933bebf64234b9bdc246b
Thakur Milan <thakurmilan42@gmail.com>
2023-08-08 18:00:52 +0530

    Added link for projects

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -17,7 +17,7 @@ export const projectData = [
       "Olx clone uses key features of Olx such as upload product,Edit product info, Like product, Search product. This uses mongoDb database and Express js for Api, and all the routes are protected with JWT.",
     img: "./images/olx_clone.jpg",
     link: "https://github.com/mila9n/olx_clone",
-    live: "https://olx-clone-mern.vercel.app/",
+    live: "https://olx-clone-mern-mila9n.vercel.app/",
   },
   {
     title: "Contact Us",
